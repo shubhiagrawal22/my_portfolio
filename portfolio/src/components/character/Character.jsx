22cd3a2cef3a7050e9dea9948aade3fbfc22edab
Shubhi Agrawal <shubhi.agrawal_cs19@gla.ac.in>
2024-02-12 19:37:55 +0530

    character component

---
+++ portfolio/src/components/character/Character.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./character.css";
+import { ReactTyped } from "react-typed";
+import reactI from '../../assets/React-icon.png';
+import girl from '../../assets/unnamed.jpg';
+import html from '../../assets/html.png';
+import css from '../../assets/css.png';
+import js from '../../assets/js.png';
+
+
+const Character = () => {
+  return (
+    <section className="char-container">
+      <div className="char-content">
+        <h2>Hi, I'm Shubhi</h2>
+        <p>
+          and I'm a passionate 
+          <span>{" "}
+            <ReactTyped
+              strings={[
+                "Software Developer",
+                "Fullstack Developer",
+                "Frontend Developer",
+              ]}
+              typeSpeed={40}
+              backSpeed={50}
+              loop
+            />
+          </span>
+        </p>
+      </div>
+      <div className="char-img">
+        <div>
+          <div className="tech-icon">
+            <img src={reactI} alt="" />
+          </div>
+          <img className="girl" src={girl} alt="" />
+        </div>
+        <div>
+          <div className="tech-icon">
+            <img src={html} alt="" />
+          </div>
+          <div className="tech-icon">
+            <img src={css} alt="" />
+          </div>
+          <div className="tech-icon">
+            <img src={js} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Character;
